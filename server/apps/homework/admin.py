29508e03b0775b31b3c1757b9b37abad714e3ad8
Michael Brauweiler <michael.brauweiler@posteo.de>
2020-11-18 19:15:34 +0100

    Improve admin panel

--- server/apps/homework/admin.py
+++ server/apps/homework/admin.py
@@ -11,7 +11,7 @@ from .models import Exercise, ExerciseResource, Submission
 
 class ExerciseResourceInline(admin.TabularInline):
     model = ExerciseResource
-    fields = ["title", "file", "resource_link", "loaded"]
+    fields = ["title", "file", "loaded", "resource_link"]
     readonly_fields = ["resource_link"]
     extra = 1
 
