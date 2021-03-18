7a744cee11cb20911612eca164604e0da690e790
Michael Brauweiler <michael.brauweiler@posteo.de>
2021-03-18 05:14:34 +0100

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Button, Container, Form, Navbar } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import { Link, Redirect, Route, Switch } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Button, Container, Form, Navbar} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
+import {Link, Redirect, Route, Switch} from "react-router-dom";
 
-import { logIn, logOut, isLoggedIn } from "./services/AuthService";
+import {logIn, logOut, isLoggedIn} from "./services/AuthService";
 
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
@@ -59,76 +59,69 @@ function App() {
                     <Route
                         exact
                         path="/"
-                        render={() => (
-                            <div className="middle-center">
-                                <h1 className="landing logo">
-                                    Homework Checker
+                    >
+                        <div className="middle-center">
+                            <h1 className="landing logo">
+                                Homework Checker
                                 </h1>
-                                {!loggedIn && (
-                                    <Link
-                                        id="signUp"
-                                        className="btn btn-primary mx-2"
-                                        to="/signup"
-                                    >
-                                        Sign up
-                                    </Link>
-                                )}
-                                {!loggedIn && (
-                                    <Link
-                                        id="logIn"
-                                        className="btn btn-primary mx-2"
-                                        to="/login"
-                                    >
-                                        Log in
-                                    </Link>
-                                )}
-                            </div>
-                        )}
-                    />
+                            {!loggedIn && (
+                                <Link
+                                    id="signUp"
+                                    className="btn btn-primary mx-2"
+                                    to="/signup"
+                                >
+                                    Sign up
+                                </Link>
+                            )}
+                            {!loggedIn && (
+                                <Link
+                                    id="logIn"
+                                    className="btn btn-primary mx-2"
+                                    to="/login"
+                                >
+                                    Log in
+                                </Link>
+                            )}
+                        </div>
+                    </Route>
                     <Route
                         path="/signup"
-                        render={() =>
-                            loggedIn ? <Redirect to="/" /> : <SignUp />
-                        }
-                    />
+                    >
+                        loggedIn ? <Redirect to="/" /> : <SignUp />
+                    </Route>
                     <Route
                         path="/login"
-                        render={() =>
-                            loggedIn ? (
+                    >
+                        loggedIn ? (
                                 <Redirect to="/" />
                             ) : (
                                 <LogIn
-                                    logIn={(email, password) =>
-                                        logIn(email, password, setLoggedIn)
-                                    }
-                                />
+                            logIn={(email, password) =>
+                                logIn(email, password, setLoggedIn)
+                            }
+                        />
                             )
-                        }
-                    />
+                    </Route>
                     <Route
                         path="/profile/"
-                        render={() =>
-                            loggedIn ? <Profile /> : <Redirect to="/" />
-                        }
-                    />
+                    >
+                        loggedIn ? <Profile /> : <Redirect to="/" />
+                    </Route>
                     <Route
                         path="/lectures/:lecture_slug/:lesson_slug/"
-                        render={() =>
-                            loggedIn ? <LessonDetail /> : <Redirect to="/" />
-                        }
-                    />
+                    >
+                        loggedIn ? <LessonDetail /> : <Redirect to="/" />
+                    </Route>
                     <Route
                         path="/lectures/:lecture_slug/"
-                        render={() =>
-                            loggedIn ? <LectureDetail /> : <Redirect to="/" />
-                        }
-                    />
+                    >
+                        loggedIn ? <LectureDetail /> : <Redirect to="/" />
+                    </Route>
                     <Route
                         path="/lectures/"
-                        render={() =>
-                            loggedIn ? <Lectures /> : <Redirect to="/" />
-                        }
-                    />
+                    >
+                        loggedIn ? <Lectures /> : <Redirect to="/" />
+                    </Route>
                 </Switch>
             </Container>
         </>
