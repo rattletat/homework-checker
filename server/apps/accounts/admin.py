b30956f265d8c785d0e638acffd9570f43671213
Michael Brauweiler <michael.brauweiler@posteo.de>
2021-03-20 07:43:34 +0100

    Improve group admin handling

--- server/apps/accounts/admin.py
+++ server/apps/accounts/admin.py
@@ -3,6 +3,9 @@ from django.contrib.auth.admin import UserAdmin
 
 from apps.teaching.models import Lecture
 from .models import CustomUser
+from django import forms
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.contrib.auth.models import Group
 
 
 class LectureInline(admin.TabularInline):
@@ -47,3 +50,49 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+# Create ModelForm based on the Group model.
+class GroupAdminForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        exclude = []
+
+    # Add the users field.
+    users = forms.ModelMultipleChoiceField(
+         queryset=CustomUser.objects.all(),
+         required=False,
+         # Use the pretty 'filter_horizontal widget'.
+         widget=FilteredSelectMultiple('users', False)
+    )
+
+    def __init__(self, *args, **kwargs):
+        # Do the normal form initialisation.
+        super(GroupAdminForm, self).__init__(*args, **kwargs)
+        # If it is an existing group (saved objects have a pk).
+        if self.instance.pk:
+            # Populate the users field with the current Group users.
+            self.fields['users'].initial = self.instance.user_set.all()
+
+    def save_m2m(self):
+        # Add the users to the Group.
+        self.instance.user_set.set(self.cleaned_data['users'])
+
+    def save(self, *args, **kwargs):
+        # Default save
+        instance = super(GroupAdminForm, self).save()
+        # Save many-to-many data
+        self.save_m2m()
+        return instance
+
+# Unregister the original Group admin.
+admin.site.unregister(Group)
+
+# Create a new Group admin.
+class GroupAdmin(admin.ModelAdmin):
+    # Use our custom form.
+    form = GroupAdminForm
+    # Filter permissions horizontal as well.
+    filter_horizontal = ['permissions']
+
+# Register the new Group ModelAdmin.
+admin.site.register(Group, GroupAdmin)
