daa071321dccc0156afc42626c251b26fd9b58f8
Michael Brauweiler <michael.brauweiler@posteo.de>
2021-03-23 09:02:45 +0100

    Support gfm footnotes and tables

--- client/src/services/MarkdownService.js
+++ client/src/services/MarkdownService.js
@@ -1,27 +1,32 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+
+import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
+import {dracula} from "react-syntax-highlighter/dist/esm/styles/prism";
+
+import TeX from "@matejmazur/react-katex";
+import "katex/dist/katex.min.css";
+
 import gfm from "remark-gfm";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
-import 'katex/dist/katex.min.css';
-import TeX from '@matejmazur/react-katex';
 import math from "remark-math";
+import footnotes from "remark-footnotes";
 
 const _renderers = {
-    inlineMath: ({ value }) => <TeX math={value} />,
-    math: ({ value }) => <TeX math={value} block />,
-    code: ({ language, value }) => {
-        return (
-            <SyntaxHighlighter
-                style={dracula}
-                language={language}
-                children={value}
-            />
-        );
+    inlineMath: ({value}) => <TeX math={value} />,
+    math: ({value}) => <TeX math={value} block />,
+    code: ({language, value}) =>
+        <SyntaxHighlighter
+            style={dracula}
+            language={language}
+            children={value}
+        />
+    ,
+    table: (props) => {
+        return <table className="table table-striped">{props.children}</table>
     }
 };
 
-const _plugins = [gfm, math];
+const _plugins = [gfm, math, footnotes];
 
 const Markdown = props => (
     <ReactMarkdown renderers={_renderers} plugins={_plugins} {...props} />
