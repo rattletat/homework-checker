e7c8622873648df96a57b7bb444d4047a7e518f8
Michael Brauweiler <michael.brauweiler@posteo.de>
2021-04-05 16:52:59 +0200

    Add footnote support

--- client/src/services/MarkdownService.js
+++ client/src/services/MarkdownService.js
@@ -10,6 +10,7 @@ import "katex/dist/katex.min.css";
 import gfm from "remark-gfm";
 import math from "remark-math";
 import smartypants from "@silvenon/remark-smartypants";
+import footnotes from "remark-footnotes";
 
 const _renderers = {
     inlineMath: ({value}) => <TeX math={value} />,
@@ -26,10 +27,21 @@ const _renderers = {
     },
     tableHead: (props) => {
         return <thead className="thead-dark">{props.children}</thead>
-    }
+    },
+    footnoteReference: (props) =>
+        <sup id={"ref-" + props.identifier}>
+            <a href={"#def-" + props.identifier}>{props.label}</a>
+        </sup>
+    ,
+    footnoteDefinition: (props) =>
+        <div className="footnoteDefinition" id={"def-" + props.identifier}>
+            <a className="backToRef" href={"#ref-" + props.identifier}>{props.label}</a>
+            <div className="footnoteBody">{props.children}</div>
+        </div>
 };
 
-const _plugins = [[gfm, {singleTilde: false}], math, [smartypants, {dashes: 'oldschool'}]];
+
+const _plugins = [[gfm, {singleTilde: false}], math, [smartypants, {dashes: 'oldschool'}], footnotes];
 
 const Markdown = props => (
     <ReactMarkdown renderers={_renderers} plugins={_plugins} {...props} />
