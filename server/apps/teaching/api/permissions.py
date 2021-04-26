3bd52baeaa90334b0973a1108ed48293c464689b
Michael Brauweiler <michael.brauweiler@posteo.de>
2021-04-26 22:20:34 +0200

    Remove debug messages

--- server/apps/teaching/api/permissions.py
+++ server/apps/teaching/api/permissions.py
@@ -33,19 +33,14 @@ class IsNotWaiting(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         user = request.user
-        print("USER: ", str(user.is_staff))
         if user.is_anonymous:
             return False
         if user.is_staff:
             return True
 
         if isinstance(obj, (Lecture, Lesson)):
-            print("STATUS1: ", str(obj.status))
-            print("VALUE: ", obj.status != "WAITING")
             return obj.status != "WAITING"
         if isinstance(obj, LectureResource):
-            print("STATUS2: ", str(obj.lecture.status))
             return obj.lecture.status != "WAITING"
         if isinstance(obj, LessonResource, Exercise):
-            print("STATUS3: ", str(obj.lesson.status))
             return obj.lesson.status != "WAITING"
