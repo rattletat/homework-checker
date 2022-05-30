8fd1adc49391d39b5141e35c471957e7929cdc8a
Michael Brauweiler <michael.brauweiler@posteo.de>
2022-05-30 21:28:27 +0100

    Change headers in admin portal

--- server/config/urls.py
+++ server/config/urls.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 import django_rq
 from django.urls import include, path
 
+admin.site.site_header = 'Homework Checker'
+admin.site.index_title = 'Admin Portal'
+admin.site.site_title = 'Homework Checker'
+
 urlpatterns = [
     path("api/", include(api_urls)),
     path(settings.ADMIN_URL, admin.site.urls),
